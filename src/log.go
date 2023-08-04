62e3af0ceb08e2a5ce463121509d864800587643
Tomasz Skrond <tomek2.skrond@gmail.com>
2023-08-05 01:14:32 +0200

    Added more readable logging

--- src/log.go
+++ src/log.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
-func GetMcServerLogs(filename string) (string, error) {
-	content, err := ioutil.ReadFile(filename)
+func GetMcServerLogs(filename string) ([]string, error) {
+	content, err := ReadLines(filename)
 	if err != nil {
 		fmt.Println("log errors")
-		return "error reading file, go back to home page", err
+		return []string{"error reading file, go back to home page"}, err
 	}
-	return string(content), err
+	return content, err
+}
+
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
